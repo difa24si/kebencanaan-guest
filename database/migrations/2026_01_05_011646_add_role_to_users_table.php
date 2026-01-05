273fb7aafcc88f778719fe207c2fae3cf0f21306
difa24si <difa24si@mahasiswa.pcr.ac.id>
2026-01-05 08:17:53 +0700

    tes

--- database/migrations/2026_01_05_011646_add_role_to_users_table.php
+++ database/migrations/2026_01_05_011646_add_role_to_users_table.php
@@ -9,20 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('user')->after('email');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
+
 };
